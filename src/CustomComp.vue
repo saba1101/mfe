66568800191168154f06d333621e7d8125a8aa7b
saba2446 <sabakhara44@gmail.com>
2022-06-08 00:18:47 +0400

    comp ++

--- src/CustomComp.vue
+++ src/CustomComp.vue
@@ -1,10 +1,30 @@
 <template>
     <div class="wrapper">
-        <h1>remotely accessable component</h1>    
+        <div class="comp">
+            <h1>MF</h1>
+            <h2>Remotly Accessed Component</h2>
+        </div>
     </div>    
 </template>
 
 <style scoped>
+
+.wrapper{
+    width: 400px;
+    height: 200px;
+    background: slategrey;
+    display: grid;
+    place-items: center;
+    text-align: center;
+}
+
+h2{
+    color: #fff;
+}
+
+h1{
+    color: lime;
+}
 
 </style>
 
