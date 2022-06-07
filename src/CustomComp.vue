79053fd04547bad7eccd1d6975703c9342d6e505
saba2446 <sabakhara44@gmail.com>
2022-06-08 00:09:18 +0400

    ..

---
+++ src/CustomComp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="wrapper">
+        <h1>remotely accessable component</h1>    
+    </div>    
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+
+
+</script>
